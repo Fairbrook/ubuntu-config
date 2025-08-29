7af801a47e235adec7b1960b06bb2e63d118c0b4
Kevin Martinez <kevinvr@hotmail.es>
2025-08-29 08:33:31 -0600

    Changed to more standard i3 installation

--- install_i3.sh
+++ install_i3.sh
@@ -1,14 +1,11 @@
+#!/bin/bash
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2025.03.09_all.deb keyring.deb SHA256:2c2601e6053d5c68c2c60bcd088fa9797acec5f285151d46de9c830aaba6173c
+sudo apt install ./keyring.deb
+echo "deb [signed-by=/usr/share/keyrings/sur5r-keyring.gpg] http://debian.sur5r.net/i3/ $(grep '^VERSION_CODENAME=' /etc/os-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install i3
+
 sudo wget -O /usr/bin/i3exit https://gist.githubusercontent.com/Fairbrook/40d04fed59068d3be57a7014c9085987/raw/4d18543b7ea4fc035de1f3d2cf06411b5943f4b4/i3exit
 sudo chmod +x /usr/bin/i3exit
 
-sh <(curl -L https://nixos.org/nix/install) --daemon
-nix-env -iA nixpkgs.xidlehook
-sudo wget -O /usr/bin/idlelock https://gist.githubusercontent.com/Fairbrook/cffa64e0e2badb3bdd935b7f7dbb75bd/raw/9335e110aa13dfc8f51d2290a60216ede3e6d0ac/idlelock
-sudo chmod +x /usr/bin/idlelock
-
-pip install "cython<3.0.0" wheel && pip install pyyaml==5.4.1 --no-build-isolation
-git clone https://github.com/Fairbrook/i3wm-themer.git
-cd i3wm-themer/
-./install_ubuntu.sh
-python3 i3wm-themer.py --config config.yaml --load themes/007.json
-cd ..
+sudo apt install -y polybar rofi
--- install_nvim.sh
+++ install_nvim.sh
@@ -1,14 +1,12 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-npm install -g eslint_d @fsouza/prettierd
-# clone
-git clone https://github.com/powerline/fonts.git --depth=1
-# install
-cd fonts
-./install.sh
-# clean-up a bit
+git clone --depth 1 https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 cd ..
-rm -rf fonts
-# Config files
-sudo rm -r ~/.config/nvim
-git clone https://github.com/Fairbrook/nvim_config.git ~/.config/nvim
+
+git clone https://github.com/Fairbrook/dotfiles.git
+cd dotfiles
+./install.sh
+cd ..
+
+sudo rm -rf neovim dotfiles
